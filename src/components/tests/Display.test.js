7458b4941b3bc47cd1211c02e95e838da4a45a4e
ambrojessica <ambrojessica0@gmail.com>
2021-12-13 19:16:29 -0600

    done

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
+//import
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
+const testShow = {
+  name: 'testing show',
+  summary: 'testing summary',
+  seasons: [
+    {
+      id: 0,
+      name: 'season 1',
+      episodes: []
+    },
+    {
+      id: 1,
+      name: 'season 2',
+      episodes: []
+    },
+  ]
+};
 
-test('renders without errors with no props', ()=>{
-
+test('renders without errors with no props', () => {
+  render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+  const buttonClicked = screen.getByRole('button');
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+  userEvent.click(buttonClicked);
+
+  const showCont = await screen.findByTestId('show-container');
+
+  expect(showCont).toBeInTheDocument();
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+
+  const button = screen.getByRole('button');
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const seasonChoice = screen.queryAllByTestId('season-option');
+    expect(seasonChoice).toHaveLength(2);
+  });
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  const displayFunc = jest.fn();
+
+  render(<Display displayFunc={displayFunc} />);
+
+  const button = screen.getByRole('button');
+  userEvent.click(button);
+  // console.log(button);
+
+  await waitFor(() => {
+    expect(displayFunc).toHaveBeenCalled();
+  });
+
+});
